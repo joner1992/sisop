43bee3a40430c6ac75defe881f1e7af922256cb7
joner1992 <filipejoner@gmail.com>
2016-09-13 23:49:42 -0300

    atualizados

--- src/cthread.c
+++ src/cthread.c
@@ -9,11 +9,10 @@
 #include <ucontext.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdint.h> // Usada para gerar ticket com uint8_t
 
 #define SUCCESS 0
 #define ERROR -1
-
-
 
 #define	CRIACAO	0
 #define	APTO	1
@@ -24,18 +23,68 @@
 #define stackSize SIGSTKSZ
 
 
-PFILA2 filaAptos = NULL;
-int tid = 1; //Mantém tid global para enumerar threads
-
 char groupName[] = "Nome: Filipe Joner | Cartao: 208840 \nNome: Jean Andrade | Cartao: \nNome: Rodrigo Dal Ri | Cartao: 244936";
 
 
+/************
+* VARIÁVEIS GLOBAIS
+************/
+
+int tid = 1; //Mantém tid global para enumerar threads
+
+PFILA2 filaAptos = NULL;
+PFILA2 filaBloqueados = NULL;
+int readyQueueCreated = 0;
+int blockedQueueCreated = 0;
+
+TCB_t *mainContext = NULL;
+int mainContextCreated = 0;
+
+/**************
+* FUNÇÕES AUXILIARES
+**************/
+int generateTicket(){
+	unsigned int random = Random2();
+	uint8_t converter = random;
+	int ticket = converter;
+	return ticket;
+}
+
+int createMainContext(){
+
+
+	return SUCCESS;
+}
+
+int createReadyQueue(){
+	mainContextCreated = createMainContext();
+
+
+	return SUCCESS;
+}
+
+
+int createBlockedQueue(){
+
+
+
+	return SUCCESS;
+}
+
+
+/****************
+*****************/
 int ccreate(void* (*start)(void*), void *arg) {
+
+	if(readyQueueCreated == 0){
+		readyQueueCreated = createReadyQueue();
+	}
+
 
 	TCB_t *newThread = (TCB_t*) malloc(sizeof(TCB_t));
 	newThread->tid = tid;
 	newThread->state = APTO;
-	newThread->ticket = 1; // Valor dummie
+	newThread->ticket = generateTicket(); // Valor dummie
 
 	getcontext(&newThread->context);
 
@@ -52,7 +101,7 @@ int ccreate(void* (*start)(void*), void *arg) {
 
 
 	tid++;
-	printf("Criou THREAD DE TID: %d\n", newThread->tid);
+	printf("Criou THREAD DE TID: %d | TICKET: %d\n",newThread->tid, newThread->ticket);
 
 
 	return newThread->tid;
---
+++ testes/testTickets.c
@@ -0,0 +1,27 @@
+/**
+** Teste da função generateTicket
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/support.h"
+#include "../include/cthread.h"
+
+void* ImprimeDez(void *arg) {
+
+	printf("Works?");
+	return (void*)NULL;
+}
+
+
+int main(int argc, char *argv[]){
+
+	int valor = atoi(argv[0]);
+	int i=0;
+	for(i; i< 100; i++){
+		int a = ccreate(ImprimeDez, (void*)&valor);
+	}
+
+	return 0;
+}
